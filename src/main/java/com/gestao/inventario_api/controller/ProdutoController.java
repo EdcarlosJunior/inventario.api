17487ee337f2c5d78607b6e40aea5790e5c3215e
José Edcarlos Miranda da Silva Junior <edcarlosjunior97@gmail.com>
2026-04-21 13:08:18 -0300

    Implementação do DELETE: removendo produtos por ID

--- src/main/java/com/gestao/inventario_api/controller/ProdutoController.java
+++ src/main/java/com/gestao/inventario_api/controller/ProdutoController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.gestao.inventario_api.model.Produto;
 import com.gestao.inventario_api.repository.ProdutoRepository;
@@ -33,4 +35,10 @@ public class ProdutoController {
         // Salvamos o produto recebido e o Spring retorna o objeto com o ID preenchido
         return repository.save(produto);
     }
+    
+    @DeleteMapping("/{id}")
+    public void excluir(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+    
 }
